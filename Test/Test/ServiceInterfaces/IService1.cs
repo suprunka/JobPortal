919af4590e23c046db97d21b3b4e19982a85e4d2
Patrycja Maria Suprun <1067712@ucn.dk>
2018-10-29 12:34:53 +0100

    delete old stuff

--- Test/Test/ServiceInterfaces/IService1.cs
+++ Test/Test/ServiceInterfaces/IService1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceLibrary.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -11,35 +12,5 @@ namespace ServiceLibrary
     [ServiceContract]
     public interface IService1
     {
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Test.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
