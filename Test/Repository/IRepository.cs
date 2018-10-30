ba4e5c27ab59dd7cf19e858fb2e3cb0b0b021728
Jakub Kida <1067677@ucn.dk>
2018-10-30 11:41:52 +0100

    T

---
+++ Test/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Repositories
+{
+     public interface IRepository<T> where T : class
+    {
+       T Create(T obj);
+        T Get(int id);
+        bool Update(T obj);
+        bool Delete(int id);
+        IQueryable<T> GetAll();
+
+    }
+}
---
+++ Test/Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public T Create(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
