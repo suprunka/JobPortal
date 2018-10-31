92867c138e455002b04631a896f58610797da8d8
Jakub Kida <1067677@ucn.dk>
2018-10-31 11:52:42 +0100

    Implement RegularExpression

--- Test/Test/Models/User.cs
+++ Test/Test/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -10,26 +11,54 @@ namespace ServiceLibrary.Models
     [DataContract]
     public class User
     {
-        [DataMember] // should have 8 digit ^[0-9]{8}$
-        public virtual String PhoneNumber { get; set; } 
-        [DataMember] //include english alphabet and danish signs
-        public String FirstName { get; set; }
-        [DataMember] //same as first name
-        public String LastName { get; set; }
-        [DataMember] //have '@' sign  ^[a-zA-Z0-9ÆæØøÅå.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$
-        public String Email { get; set; }
-        [DataMember] //
-        public String UserName { get; set; }
-        [DataMember] //have capital, small letters and number ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}$
-        public String Password { get; set; }
-        [DataMember] 
-        public String AddressLine { get; set; }
         [DataMember]
+        [Required]
+        [RegularExpression ("^[0-9]{8}$")]
+        public virtual String PhoneNumber { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]{1,}$")]
+        public String FirstName { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]{1,}$")]
+        public String LastName { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
+        public String Email { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]{1,}$")]
+        public String UserName { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}$")]
+        public String Password { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]{1,}$")]
+        public String AddressLine { get; set; }
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9ÆæØøÅå]{1,}$")]
         public String CityName { get; set; }
-        [DataMember] //should be 4 digit ^[0-9]{4}$
+
+        [DataMember]
+        [Required]
+        [RegularExpression("^[0-9]{4}$")]
         public String Postcode { get; set; }
+
         [DataMember] //enum
         public Region Region { get; set; }
+
         [DataMember]
         public Gender Gender { get; set; }
     }
