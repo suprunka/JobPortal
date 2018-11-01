b9daa1b2837c8c816a02fd0f3907c4d46c770f38
Patrycja Maria Suprun <1067712@ucn.dk>
2018-11-01 09:23:09 +0100

    bu

---
+++ Test/AppJobPortal/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJobPortal.Controllers
+{
+    public class UserController
+    {
+        public UserController() { }
+    }
+}
