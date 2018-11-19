8efd75e0099b050d8273859b24d0630246faf65a
Jakub Kida <1067677@ucn.dk>
2018-11-19 15:53:11 +0100

    vxc

--- Test/MyWeb/Controllers/UserController.cs
+++ Test/MyWeb/Controllers/UserController.cs
@@ -71,10 +71,14 @@ namespace MyWeb.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult> UserProfile(string id)
-        { 
-           return View(UserMapping.Map_User_To_UserProfileViewModel(await _proxy.FindUserAsync(id)));
+        public ActionResult UserProfile(string id)
+        {
+            UserProfileViewModel user = UserMapping.Map_User_To_UserProfileViewModel(_proxy.FindUser(id));
+            user.Services = _offerProxy.GetAllOffers().Where(x => x.AuthorId == id).Select(x => new ManageOffers { Id = x.Id, Author = x.AuthorId, Description = x.Description, RatePerHour = x.RatePerHour, Title = x.Title, Subcategory = x.Subcategory, Category = x.Category }).ToArray();
+
+            return View(user);
         }
+
 
         [HttpGet]
         public async Task<ActionResult> DeleteAsync(int id)
