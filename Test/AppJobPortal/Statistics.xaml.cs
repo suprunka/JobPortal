ecc1b3e13e3867df3917ce60916fb5629a9df98b
Patka <loca.chili@gmail.com>
2018-12-06 11:43:11 +0100

    mm

--- Test/AppJobPortal/Statistics.xaml.cs
+++ Test/AppJobPortal/Statistics.xaml.cs
@@ -18,48 +18,40 @@ namespace AppJobPortal
     /// </summary>
     public partial class Statistics : UserControl
     {
-       
-public string[] Labels { get; set; }
-        public Func<double, string> Formatter { get; set; }
-        private readonly IOfferService _proxy;
-        private SeriesCollection SeriesCollection;
+        private IOfferService _offerproxy;
 
         public Statistics()
         {
 
             InitializeComponent();
-            _proxy = new OfferServiceClient("OfferServiceTcpEndpoint");
-
-            var home = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.Home).Count();//.Where(x => x.RatePerHour > 200).Count();
-            var architecture = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.Architecture).Count(); //.Where(x => x.RatePerHour > 200).Count();
-            var it = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.IT).Count();//.Where(x => x.RatePerHour > 200).Count();
-            var media = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.Media).Where(x => x.RatePerHour > 200).Count();
-            var repairs = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.Repairs).Where(x => x.RatePerHour > 200).Count();
-            var tutoring = _proxy.GetAllOffers().Where(x => x.Category == JobPortal.Model.Category.Tutoring).Where(x => x.RatePerHour > 200).Count();
-
+            //_offerproxy = new OfferServiceClient()
             SeriesCollection = new SeriesCollection
             {
                 new ColumnSeries
                 {
-                    Title = "Below 200 kr/h",
-                    Values = new ChartValues<double> { home, architecture, it }
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
                 }
             };
 
             //adding series will update and animate the chart automatically
             SeriesCollection.Add(new ColumnSeries
             {
-                Title = "Abowe 200 kr/h",
-                Values = new ChartValues<double> { 11, 26, 32, 22, 2, 21 }
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42,10 }
             });
 
             //also adding values updates and animates the chart automatically
-            SeriesCollection[1].Values.Add(48d);
 
-            Labels = new[] { "Home", "Architecture", "IT" };
+            Labels = new[] { "Home", "IT", "Tutoring", "Media" };
+            Formatter = value => value.ToString("N");
 
             DataContext = this;
         }
-       
-}
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
     }
+}
